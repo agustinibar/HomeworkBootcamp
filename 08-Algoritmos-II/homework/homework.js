7c4d21371a751a1f7505a2fa97b93c1f9ab66628
agustinibar <agustinibarperrotta@gmail.com>
2023-05-15 13:08:34 -0300

    algoritmos 2

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -3,20 +3,64 @@
 
 function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
-  // el array recibido como parámetro
+  // el array recibido como parámetro                                                    
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length  < 2)return array; 
+  let random = Math.floor(Math.random() * array.length);
+  let pivot = array.splice(random, 1)[0];
+  let left = [];
+  let right = [];
 
-}
+  for(let i=0; i<array.length; i++){
+    if(array[i] < pivot){
+      left.push(array[i]);
+    }else {
+      right.push(array[i]); 
+  }
+  };
+
+  left = quickSort(left); 
+  right = quickSort(right);
+  
+  array = left.concat([pivot]).concat(right);
+
+  return array
+};
+
+let array = [9,8,7,2,6,4];
+console.log(quickSort(array))
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length < 2) return array;
 
+  const mitad = Math.floor(array.length / 2);
+  let left = array.slice(0, mitad);
+  let right = array.slice(mitad);
+
+  array = [];
+
+  left = mergeSort(left);
+  right= mergeSort(right);
+
+  while(left.length && right.length){
+    if(left[0]<right[0]){
+      array.push(left.shift())
+    } else {
+      array.push(right.shift())
+    }
+  };
+  
+  array = array.concat(left, right);
+  return array;
   //okey probemos ahora
-}
+};
+let arrayMerge = [9,5,1,3,7,6,2];
+console.log(mergeSort(arrayMerge))
 
 // No modificar nada debajo de esta línea
 // --------------------------------
