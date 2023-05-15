e237190b166451fb683536b8356edf058ab2e44d
agustinibar <agustinibarperrotta@gmail.com>
2023-05-15 13:07:21 -0300

    Estructura de datos 1

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -33,7 +33,7 @@ console.log(nFibonacci(9))
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
-  - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
+  - dequeue: remueve un valor respetando el orden. Retorna  undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
 
 Pueden utilizar class o función constructora.
