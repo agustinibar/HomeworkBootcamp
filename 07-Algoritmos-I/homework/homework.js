43e21352fd3bd8e61434bf5c019495e6c6c94a1c
agustinibar <agustinibarperrotta@gmail.com>
2023-05-16 19:33:57 -0300

    algoritmos 1

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,8 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let array  = [1]; 
 
-}                                                                                         
+  for(let i = 2; i<=num; i++){
+    while(num % i === 0){
+      array.push(i);
+      num /= i;
+    }
+  }
+  return array
+  
+ 
+};                                                                                      
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
@@ -55,9 +65,23 @@ function selectionSort(array) {
   // Devolver el array ordenado resultante  
   // Tu código:                                  
  
+  for(let i = 0; i<array.length -1; i++){ // recorro el array dede cero hasta la anteultima posicion
+    let minIndex = i; //declaro la primer iteracion como el numero mas chico
+    for(let j = i+1; j<array.length; j++){//recorro el array desde la primer posicion hasta la ultima, aqui se forman los dos punteros. El que marca el minimo y el que compara. 
+      if (array[minIndex] > array[j]){//si el puntero comparador, es mas chico que el ultimo elemento guardado 
+        minIndex = j; // si el puntero comparador es mas chico, pisamos la variable minIndex con el nuevo valor mas chico encontrado
+      }
+    }
+    if(i!==minIndex){//terminado el recorrido del puntero, el minIndex tiene el valor de la iteracion mas chica que encontro el puntero o la misma (porque no encontro uno mas chico), si no es la misma entonces: 
+      let temp = array[i]; // alojamos la iteracion actual en una variable para no perder su valor, la iteracion actual va a ser la primera sin ordenar, la que esta apuntando el primer puntero 
+      array[i] = array[minIndex]; //intercambiamos lo que hay en la posicion del puntero inicial con lo que el puntero comparativo encontro mas chico
+      array[minIndex] = temp; //la variable minIndex ahora sera igual al puntero inicial, el que marca el primer elemento del array.
+    };
+}; return array
 
 };
 let arrayPrueba = [10,1,3,8]; 
+console.log(arrayPrueba)
 console.log(selectionSort(arrayPrueba))
 
 
